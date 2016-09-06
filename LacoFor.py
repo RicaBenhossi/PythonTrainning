b6509cae5e2e4f44e89141545b44dbece7e53fb2
ricardo <ricardo@logusretail.com.br>
2016-09-06 14:37:41 -0300

    For Lesson 1

---
+++ LacoFor.py
@@ -0,0 +1,11 @@
+""" Laço de Repetição FOR
+
+-SINTAXE - for contador in limite: 
+               comando a cada contador 
+"""
+
+""" Laços de Repetição com Dicionários e Listas """
+# Para exibir o conteúdo, use a variável de controle
+names = ['Adam', 'Alex', 'Mariah', 'Martine', 'Columbus']
+for item in names:
+    print(item)
