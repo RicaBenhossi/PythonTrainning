8824ed46484f9b0b0da7ae951efe4d7b3b957a5e
ricardo <ricardo@logusretail.com.br>
2016-09-27 14:47:12 -0300

    adjusts for run correctly on python 3.5

--- Battleship.py
+++ Battleship.py
@@ -3,13 +3,13 @@ from random import randint
 board = []
 
 for x in range(5):
-    board.append(["O"] * 5)
+    board.append(['O'] * 5)
 
 def print_board(board):
     for row in board:
-        print(" ".join(row))
+        print(' '.join(row))
 
-print("Vamos jogar Batalha Naval!")
+print('Vamos jogar Batalha Naval!')
 print_board(board)
 
 def random_row(board):
@@ -26,22 +26,22 @@ print(ship_col)
 # Tudo a partir daqui deve ir dentro do laco!
 # Tenha certeza de recuar quatro espacos!
 for turn in (range(4)):
-    guess_row = int(input("Adivinhe a Linha:"))
-    guess_col = int(input("Adivinhe a Coluna:"))
+    guess_row = int(input('Adivinhe a Linha:'))
+    guess_col = int(input('Adivinhe a Coluna:'))
     
     if guess_row == ship_row and guess_col == ship_col:
-        print("Parabens, voce afundou meu couracado!")
+        print('Parabens, voce afundou meu couracado!')
         break
     else:
         if turn == 3:
-            print("Game Over")
+            print('Game Over')
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("Oops, isso nao e nem mesmo no oceano.")
-        elif(board[guess_row][guess_col] == "X"):
-            print("Voce ja tentou esse.")
+            print('Oops, isso nao e nem mesmo no oceano.')
+        elif(board[guess_row][guess_col] == 'X'):
+            print('Voce ja tentou esse.')
         else:
-            print("Voce errou meu couracado!")
-            board[guess_row][guess_col] = "X"
+            print('Voce errou meu couracado!')
+            board[guess_row][guess_col] = 'X'
         # Exiba aqui (turn + 1)!
     print(turn + 1)
     print_board(board)
