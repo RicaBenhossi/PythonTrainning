13b1d2b6119469b45411a22c348843289e19ead5
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2016-10-25 00:37:07 -0200

    text reverse

--- teste.py
+++ teste.py
@@ -1,2 +1,10 @@
+
 def reverse(text):
-    
+    y = len(text)
+    for x in text:
+        revtext[y] = x
+        y -= 1
+    return revtext
+
+x = input(str('Type the textbyou want to reverse: '))
+print(reverse(x))
