54abe24d76db83106d28738524af857d7e44790d
ricardo <ricardo@logusretail.com.br>
2016-11-29 14:43:41 -0200

    New document: Median.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-def median(numbers):
-    numbers = sorted(numbers)
-    position = int(len(numbers) / 2)
-    if ((position % 2) != 0):
-        return((numbers[position] + numbers[position - 1]) / 2)
-    else:
-        return(numbers[position])
-
-print(median([7, 12, 3, 1, 6]))
