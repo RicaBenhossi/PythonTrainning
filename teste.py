a0fc10bb43c9b1c839f0b20b6f04cb9f539cbe5f
ricardo <ricardo@logusretail.com.br>
2017-01-27 14:59:07 -0200

    test

---
+++ teste.py
@@ -0,0 +1,8 @@
+def print_grades(grades):
+    x = 0
+    while x < len(grades):
+        print(grades[x])
+        x += 1
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+print_grades(grades)
