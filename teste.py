31c354bb125ae904d025f0c91948fa643d0a6e4a
ricardo <ricardo@logusretail.com.br>
2017-01-31 14:39:15 -0200

    new teste.py exercise.

--- teste.py
+++ teste.py
@@ -1,8 +1,17 @@
-def print_grades(grades):
-    x = 0
-    while x < len(grades):
-        print(grades[x])
-        x += 1
+notas = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
-grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
-print_grades(grades)
+
+def grades_sum(scores):
+    total = 0
+    x = 0
+    while x < len(scores):
+        total += scores[x]
+        x += 1
+    print(total)
+    return total
+
+
+def grades_average(grades):
+    return (grades_sum(grades) / float(len(grades)))
+
+print(grades_average(notas))
