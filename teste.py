60511e1f0110d378915cf74e58aca5d1bef33c7a
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2017-05-16 14:51:06 -0300

    Correction on the test file

--- teste.py
+++ teste.py
@@ -12,5 +12,5 @@ print(my_dict.values())
 for key in my_dict:
     print(key, my_dict[key])
 
-cubes_by_four = [(x ** 3) for x in range(1, 11) if ((x ** 3) % 4 == 0)] 
+cubes_by_four = [(x ** 3) for x in range(1, 11) if ((x ** 3) % 4 == 0)]
 print(cubes_by_four)
