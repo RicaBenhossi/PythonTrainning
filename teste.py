824b9621abb93f7de8a22ff2ac3629e77ea8b184
ricardo <ricardo@logusretail.com.br>
2017-06-05 08:39:28 -0300

    Adjust

--- teste.py
+++ teste.py
@@ -1,17 +1,10 @@
-my_dict = {
-    "Name": "Natalia",
-    "Age": 31,
-    "BDFL": True
-}
+to_21 = range(1, 22)
+odds = to_21[::2]
+middle_third = to_21[(len(to_21) // 3):((len(to_21) // 3) * 2)]
 
-print(my_dict.items())
-print(my_dict.keys())
-print(my_dict.values())
+print(to_21)
+print(odds)
+print(middle_third)
 
-# New commit from home.
 
-for key in my_dict:
-    print(key, my_dict[key])
-
-cubes_by_four = [(x ** 3) for x in range(1, 11) if ((x ** 3) % 4 == 0)]
-print(cubes_by_four)
+print('Hello World')
