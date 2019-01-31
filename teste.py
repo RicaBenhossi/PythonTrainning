c3dd55a855a63116fb0476b6e66dc62f66848fb3
ricardo <ricardo@logusretail.com.br>
2019-01-31 17:25:05 -0200

    teste commit

--- teste.py
+++ teste.py
@@ -1,7 +1,3 @@
-def check_bit4(item):
-    if item > 0:
-        return "Ativo"
-    else:
-        return "off"
-
-check_bit4(int(input("Digite um númro: ")))
+print('Hello World!')
+print('teste 2')
+print('teste 3')
